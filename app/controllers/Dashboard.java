02896cbe90e415abc394267bda3629e2dd9e9497
Eamonn de Leastar <edeleastar@wit.ie>
2017-03-05 12:52:00 +0000

    create example playlist and pass to views

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.Playlist;
+import models.Song;
 import play.Logger;
 import play.mvc.Controller;
 
@@ -7,7 +9,15 @@ public class Dashboard extends Controller
 {
   public static void index() {
     Logger.info("Rendering Dashboard");
-    Logger.debug("some step has occurred...");
-    render ("dashboard.html");
+    
+    Song s1 = new Song("Piano Sonata No. 3", "Beethoven");
+    Song s2 = new Song("Piano Sonata No. 7", "Beethoven");
+    Song s3 = new Song("Piano Sonata No. 10", "Beethoven");
+    Playlist playlist = new Playlist("Beethoven Sonatas");
+    playlist.songs.add (s1);
+    playlist.songs.add (s2);
+    playlist.songs.add (s3);
+    
+    render ("dashboard.html", playlist);
   }
 }
