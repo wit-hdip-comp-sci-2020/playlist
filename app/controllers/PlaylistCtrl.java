2416a73fb0999588e51f404e70e629396d56d5f9
Eamonn de Leastar <edeleastar@wit.ie>
2017-03-09 13:26:18 +0000

    retrieve id for playlist from route and lookup database

--- app/controllers/PlaylistCtrl.java
+++ app/controllers/PlaylistCtrl.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import models.Playlist;
 import models.Song;
+import play.Logger;
 import play.mvc.Controller;
 
 public class PlaylistCtrl extends Controller
 {
-  public static void index()
+  public static void index(Long id)
   {
-    render("playlist.html");
+    Playlist playlist = Playlist.findById(id);
+    Logger.info ("Playlist id = " + id);
+    render("playlist.html", playlist);
   }
 }
