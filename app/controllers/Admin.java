1240fa5f4bc2034e418ccdc5501c617d9406cd9c
Eamonn de Leastar <edeleastar@wit.ie>
2017-03-14 06:47:48 +0000

    java style update

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -7,8 +7,9 @@ import play.mvc.Controller;
 
 public class Admin extends Controller
 {
-  public static void index() {
-   List<Song> songs = Song.findAll();
-   render ("admin.html", songs);
+  public static void index()
+  {
+    List<Song> songs = Song.findAll();
+    render("admin.html", songs);
   }
 }
